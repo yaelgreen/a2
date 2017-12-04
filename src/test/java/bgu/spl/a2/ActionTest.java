09a031e7ae32d7aa2ff42b4cd17820d713b8127d
yaelgreen <yaelgreen65@gmail.com>
2017-12-04 22:16:20 +0200

    Initial commit

---
+++ src/test/java/bgu/spl/a2/ActionTest.java
@@ -0,0 +1,43 @@
+package bgu.spl.a2;
+
+import junit.framework.TestCase;
+
+public class ActionTest extends TestCase {
+
+	public ActionTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	public void testHandle() {
+		fail("Not yet implemented");
+	}
+
+	public void testThen() {
+		fail("Not yet implemented");
+	}
+
+	public void testComplete() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testSendMessage() {
+		fail("Not yet implemented");
+	}
+
+}
