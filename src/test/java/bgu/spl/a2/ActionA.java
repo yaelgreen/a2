2be37d22dcc1c05a4495c44aa42ebbc85b1a46cf
yaelgreen <yaelgreen65@gmail.com>
2017-12-05 08:26:16 +0200

    Actor thread pool test - basic flow
    Test basic flow. based on figure 2 from assignment description

---
+++ src/test/java/bgu/spl/a2/ActionA.java
@@ -0,0 +1,14 @@
+package bgu.spl.a2;
+
+public class ActionA<R> extends Action<R> {
+	
+	public ActionA(String ActorName, String ActionName) {
+		//TODO
+	}
+
+	@Override
+	protected void start() {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/test/java/bgu/spl/a2/ActorThreadPoolTest.java
@@ -0,0 +1,51 @@
+package bgu.spl.a2;
+
+import java.util.concurrent.CountDownLatch;
+
+import junit.framework.TestCase;
+
+public class ActorThreadPoolTest extends TestCase {
+
+	public ActorThreadPoolTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	//Test basic flow. based on figure 2 from assignment description 
+	public void testFlow() {
+		ActorThreadPool actorThreadPool = new ActorThreadPool(2);
+		Action<String> action1 = new ActionA<String>("Action1", "Actor1");
+		Action<String> action2 = new ActionA<String>("Action2", "Actor1");
+		actorThreadPool.submit(action1, "Actor1", new ActorState());
+		actorThreadPool.submit(action2, "Actor1", new ActorState());
+		actorThreadPool.start();
+		CountDownLatch latch = new CountDownLatch(2);
+		action1.getResult().subscribe(() -> {
+			latch.countDown();
+		});
+		action2.getResult().subscribe(() -> {
+			latch.countDown();
+		});
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			actorThreadPool.shutdown();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
