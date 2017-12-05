cabb13bd1f4f10657d59b373c0a47a4530389861
yaelgreen <yaelgreen65@gmail.com>
2017-12-05 21:23:18 +0200

    create empty Promise test and Version monitor test

---
+++ src/test/java/bgu/spl/a2/PromiseTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package bgu.spl.a2;
+
+import junit.framework.TestCase;
+
+/**
+ * @author yaelgree
+ *
+ */
+public class PromiseTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public PromiseTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link bgu.spl.a2.Promise#get()}.
+	 */
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bgu.spl.a2.Promise#isResolved()}.
+	 */
+	public void testIsResolved() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bgu.spl.a2.Promise#resolve(java.lang.Object)}.
+	 */
+	public void testResolve() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bgu.spl.a2.Promise#subscribe(bgu.spl.a2.callback)}.
+	 */
+	public void testSubscribe() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -0,0 +1,31 @@
+package bgu.spl.a2;
+
+import junit.framework.TestCase;
+
+public class VersionMonitorTest extends TestCase {
+
+	public VersionMonitorTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testGetVersion() {
+		fail("Not yet implemented");
+	}
+
+	public void testInc() {
+		fail("Not yet implemented");
+	}
+
+	public void testAwait() {
+		fail("Not yet implemented");
+	}
+
+}
